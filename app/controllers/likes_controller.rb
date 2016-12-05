325bf0c389675a9d9e17112b1816a5d2354a2d64
北添真広 <sas.surfer0jonny@gmail.com>
2016-12-05 21:25:11 +0900

    いいね機能の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,21 +2,14 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.create(user_id: current_user.id, image_id: params[:image_id])
-    @likes = Like.where(image_id: params[:image_id])
-    # @images = image.all
-    @subjects = Subject.all
-    @reviews = Review.all
-    @images = Image.all
+    @image = @like.image
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, image_id: params[:image_id])
+    @image = like.image
     like.destroy
-    @likes = Like.where(image_id: params[:image_id])
-    # @images = image.all
-    @subjects = Subject.all
-    @reviews = Review.all
-    @images = Image.all
+    @image.likes_count -= 1
   end
 
 end
